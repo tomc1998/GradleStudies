fed5c4d3480a09e14fb41e7792b9f349f868d2ed
Thomas Cheng <thomascheng1998@googlemail.com>
2017-11-15 19:48:01 +0000

    Formatted code

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main
 {
   public static void main(String[] args)
-   {
-	System.out.println("Hello world");
+  {
+    System.out.println("Hello world");
 
-	for(int i=0 ; i<5; i++)
-	{
-		System.out.println(i);
-	}
-   }
+    for(int i=0 ; i<5; i++)
+    {
+      System.out.println(i);
+    }
+  }
 
 }
