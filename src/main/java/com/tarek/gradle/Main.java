c9c9d05a3acbc2203a6838e28711ff5800fd8ffb
Thomas Cheng <thomascheng1998@googlemail.com>
2017-11-20 21:38:10 +0000

    Moved project to gradle

---
+++ src/main/java/com/tarek/gradle/Main.java
@@ -0,0 +1,23 @@
+package com.tarek.gradle;
+
+public class Main {
+
+  /**
+   * This function adds 2 numbers together
+   * 
+   * @param a The first number to add
+   * @param b The second number to add
+   * @return The sum of the two numbers
+   */
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+
+    for(int i=0 ; i<5; i++) {
+      System.out.println(i);
+    }
+  }
+}
